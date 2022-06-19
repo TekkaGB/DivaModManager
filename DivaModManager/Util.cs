f9fb1956099b7d1d3c046aaade32d135c5ba52c5
TekkaGB <tekka.gb@gmail.com>
2022-06-19 12:33:06 -0700

    Refactored use of StrCmpLogicalW

--- DivaModManager/Util.cs
+++ DivaModManager/Util.cs
@@ -1,21 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Runtime.InteropServices;
+﻿using System.Collections.Generic;
 
 
 namespace DivaModManager
 {
     public class NaturalSort : IComparer<string>
     {
-        [DllImport("shlwapi.dll", CharSet = CharSet.Unicode)]
-        public static extern int StrCmpLogicalW(string x, string y);
-
         public int Compare(string x, string y)
         {
-            return StrCmpLogicalW(x, y);
+            if (x == null && y == null) return 0;
+            if (x == null) return -1;
+            if (y == null) return 1;
+
+            int lx = x.Length, ly = y.Length;
+
+            for (int mx = 0, my = 0; mx < lx && my < ly; mx++, my++)
+            {
+                if (char.IsDigit(x[mx]) && char.IsDigit(y[my]))
+                {
+                    long vx = 0, vy = 0;
+
+                    for (; mx < lx && char.IsDigit(x[mx]); mx++)
+                        vx = vx * 10 + x[mx] - '0';
+
+                    for (; my < ly && char.IsDigit(y[my]); my++)
+                        vy = vy * 10 + y[my] - '0';
+
+                    if (vx != vy)
+                        return vx > vy ? 1 : -1;
+                }
+
+                if (mx < lx && my < ly && x[mx] != y[my])
+                    return x[mx] > y[my] ? 1 : -1;
+            }
+
+            return lx - ly;
         }
     }
 }
