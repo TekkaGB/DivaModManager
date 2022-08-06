31656b7a89afdde1a7e9e8b8ab7d675bbf99c871
TekkaGB <tekka.gb@gmail.com>
2022-08-05 19:56:16 -0700

    Rephrased warning message

--- DivaModManager/ModUpdater.cs
+++ DivaModManager/ModUpdater.cs
@@ -300,7 +300,7 @@ namespace DivaModManager
                 }
             }
             else if (item.HasUpdates == null)
-                Global.logger.WriteLine($"{mod} has no last updated field, it is most likely trashed by the creator", LoggerType.Warning);
+                Global.logger.WriteLine($"{mod} was most likely trashed by the creator and cannot receive anymore updates", LoggerType.Warning);
         }
         private static async Task ModUpdate(DivaModArchivePost item, string mod, Metadata metadata, Progress<DownloadProgress> progress, CancellationTokenSource cancellationToken)
         {
